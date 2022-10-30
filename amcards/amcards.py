ac2b42ac1202dbedd50b828cad6f12f8e31b477d
Simone <yolodoggy911@gmail.com>
2022-10-30 01:39:19 -0700

    added send_card to client

--- amcards/amcards.py
+++ amcards/amcards.py
@@ -2,7 +2,8 @@ import requests
 from typing import List
 
 
-from .models import User, Template, Gift, Campaign
+from .models import User, Template, Gift, Campaign, CardResponse
+from . import exceptions
 from . import __helpers as helpers
 
 
@@ -52,3 +53,52 @@ class AMcardsClient:
 
         return [Campaign.from_json(campaign_json) for campaign_json in campaigns_json]
 
+    def send_card(
+        self,
+        template_id: str | int,
+        initiator: str,
+        shipping_address: dict,
+        return_address: dict = None,
+        send_date: str = None
+    ):
+        # Validate shipping details
+        missings = helpers.get_missing_required_shipping_address_fields(shipping_address)
+        if missings:
+            error_message = 'Missing the following required shipping address fields: ' + ', '.join(missings)
+            raise exceptions.ShippingAddressError(error_message)
+
+        # Validate send date
+        if send_date is not None and not helpers.is_valid_date(send_date):
+            error_message = 'Invalid send_date format, please specify send date as "YYYY-MM-DD", or omit it'
+            raise exceptions.SendDateError(error_message)
+
+        # Sanitize shipping address and return address
+        shipping_address = helpers.sanitize_shipping_address(shipping_address)
+        if return_address is not None:
+            return_address = helpers.sanitize_return_address(return_address)
+            # prefix return address fields with return_
+            return_address = {f'return_{key}': value for key, value in return_address.items()}
+
+        # Build request json payload
+        body = {
+            'template_id': template_id,
+            'initiator': initiator,
+        } | shipping_address
+
+        if return_address is not None:
+            body |= return_address
+
+        if send_date is not None:
+            body |= {'send_date': send_date}
+
+        res = requests.post(f'{DOMAIN}/cards/open-card-form-oa/', json=body, headers=self.HEADERS)
+
+        # Check for errors
+        match res.status_code:
+            case 401:
+                raise exceptions.AuthenticationError('Access token provided to client is unauthorized')
+            case 402:
+                raise exceptions.InsufficientCreditsError('Clients\' user has insufficient credits')
+
+        res_json = res.json()        
+        return CardResponse.from_json(res_json)
