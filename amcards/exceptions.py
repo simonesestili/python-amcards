8682330102464e28f3ce5a2ab3e0e2f6b7f3d6d4
Simone <yolodoggy911@gmail.com>
2022-10-30 01:38:19 -0700

    added exceptions

---
+++ amcards/exceptions.py
@@ -0,0 +1,15 @@
+class AMcardsException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class ShippingAddressError(AMcardsException, ValueError):
+    """Some shipping address fields are missing or invalid"""
+
+class AuthenticationError(AMcardsException):
+    """Access token provided to client is unauthorized"""
+
+class InsufficientCreditsError(AMcardsException):
+    """Clients' user has insufficient credits"""
+
+class SendDateError(AMcardsException, ValueError):
+    """Invalid format for send date"""
