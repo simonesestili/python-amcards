b9a867b99e715f5f4406a8e1176fbc5a63415677
Simone <yolodoggy911@gmail.com>
2022-10-31 16:36:35 -0700

    incremented version to 0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def README():
 
 setup(
     name='python-amcards',
-    version='0.0.2',
+    version='0.1.0',
     description='A wrapper for the AMcards API.',
     long_description=README(),
     long_description_content_type='text/markdown',
