3230144d74dfa344f4621f2c8b5ea44e8df46a77
simonesestili <yolodoggy911@gmail.com>
2022-11-05 02:41:26 -0700

    inc ver 1.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def README():
 
 setup(
     name='python-amcards',
-    version='1.1.1',
+    version='1.1.2',
     description='A wrapper for the AMcards API.',
     long_description=README(),
     long_description_content_type='text/x-rst',
